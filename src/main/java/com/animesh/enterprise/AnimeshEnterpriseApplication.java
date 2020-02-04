6dec57c4a1055475c476e104491256310212d9ec
animesh <aprosad@gmail.com>
2020-02-04 19:20:01 +0530

    Initial Commit

---
+++ src/main/java/com/animesh/enterprise/AnimeshEnterpriseApplication.java
@@ -0,0 +1,13 @@
+package com.animesh.enterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeshEnterpriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeshEnterpriseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/animesh/enterprise/AnimeshEnterpriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animesh.enterprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeshEnterpriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
